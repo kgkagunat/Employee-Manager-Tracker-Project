692d323a6e94531f0450be7e572ff5dccf690ac0
Jose Seto <joseseto8@gmail.com>
2023-06-26 21:30:24 -0700

    Added skeleton for verification

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,22 @@ var express = require('express');
 var passport = require('passport')
 var localStrategy = require('passport-local')
 var crypto = require('crypto')
+var user = require ('../models/User')
+
+passport.use(new localStrategy(function verify(username, password, cb) {
+    user.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+      if (err) { return cb(err); }
+      if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+  
+      crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+        if (err) { return cb(err); }
+        if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+          return cb(null, false, { message: 'Incorrect username or password.' });
+        }
+        return cb(null, row);
+      });
+    });
+}));
 
 passport.serializeUser(function(user, cb) {
     process.nextTick(function() {
