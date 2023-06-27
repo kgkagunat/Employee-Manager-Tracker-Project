03571b03af7770791f1fda76cde099fc54cdcaad
Jose Seto <joseseto8@gmail.com>
2023-06-26 21:30:37 -0700

    changed name to username

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ User.init(
             primaryKey: true,
             autoIncrement: true
         },
-        name: {
+        username: {
             type: DataTypes.STRING,
             allowNull: false
         },
