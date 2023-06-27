519ef7904af8bfd1b11a0739ec888c9b426bcd52
Jose Seto <joseseto8@gmail.com>
2023-06-26 18:56:53 -0700

    Added skeleton code to server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
