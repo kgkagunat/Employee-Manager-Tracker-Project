4a47a0b10dfe6f7ebcd322d62f2210c774ec924b
Jose Seto <joseseto8@gmail.com>
2023-06-29 19:20:26 -0700

    made changes to function exports

---
+++ utils/initialize.js
@@ -0,0 +1,32 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+
+
+function initialize(passport, getUserByEmail, getUserById){
+    const authenticateUser = async (email, password, done)=> {
+        const user = getUserByEmail(email)
+        if (user == null){
+            return done(null, false,{message: 'Invalid email or password'})
+        }
+        try{
+            if (await bcrypt.compare(password, user.password)){
+                return done(null, user)
+            } else {
+                return done(null, false, {message: 'Invalid email or password'})
+            }
+        } catch (err){
+            return done(err)
+        }
+    }
+
+    passport.user(new LocalStrategy({usernameField: 'email' },authenticateUser))
+    passport.serializeUser((user, done) =>{
+        done(null, user.id)
+    })
+    passport.deserializeUser((id, done) =>{
+        done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize;
