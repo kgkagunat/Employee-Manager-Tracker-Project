7324f8c9e5996226873d8be0a8eecbb2cf0b25f7
kgkagunat <kgkagunat@gmail.com>
2023-07-09 14:20:40 -1000

    7/9/23 2:20pm - Fixed the modal for the jobs - still working on the rest of the URD stuff

--- public/css/update-jobs.js
+++ public/css/update-jobs.js
@@ -1,48 +1,47 @@
-document.querySelector('.edit-modal .save').addEventListener('click', async (event) => {
-    event.preventDefault();
-
-    // get the current job id from the data-id attribute of the save button
-    const currentJobId = event.target.parentNode.dataset.id;
-
-    // Fetching details for the PUT request
-    const jobTitle = document.getElementById('job-title').value.trim();
-    const jobDescription = document.getElementById('job-description').value.trim();
-    const jobSalary = document.getElementById('job-salary').value.trim();
-    const jobDepartment = document.getElementById('job-department').value.trim();
-
-    const response = await fetch(`/api/jobs/${currentJobId}`, {
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.edit-modal .save').addEventListener('click', async (event) => {
+      event.preventDefault();
+  
+      const currentJobId = event.target.dataset.id;
+      const jobTitle = document.getElementById('job-title').value.trim();
+      const jobDescription = document.getElementById('job-description').value.trim();
+      const jobSalary = document.getElementById('job-salary').value.trim();
+      const jobDepartment = document.getElementById('job-department').value.trim();
+  
+      const response = await fetch(`/api/jobs/${currentJobId}`, {
         method: 'PUT',
-        body: JSON.stringify({ 
-            job_title: jobTitle,
-            job_description: jobDescription,
-            job_salary: jobSalary,
-            department_id: jobDepartment
+        body: JSON.stringify({
+          job_title: jobTitle,
+          job_description: jobDescription,
+          job_salary: jobSalary,
+          department_id: jobDepartment
         }),
         headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
+      });
+  
+      if (response.ok) {
         console.log('Job updated successfully');
         location.href = '/jobs';
-    } else {
+      } else {
         alert('Failed to update job');
-    }
-});
-
-document.querySelector('.edit-modal .delete').addEventListener('click', async (event) => {
-    event.preventDefault();
-
-    // get the current job id from the data-id attribute of the delete button
-    const currentJobId = event.target.parentNode.dataset.id;
-    
-    const response = await fetch(`/api/jobs/${currentJobId}`, {
-        method: 'DELETE',
+      }
     });
-
-    if (response.ok) {
+  
+    document.querySelector('.edit-modal .delete').addEventListener('click', async (event) => {
+      event.preventDefault();
+  
+      const currentJobId = event.target.dataset.id;
+  
+      const response = await fetch(`/api/jobs/${currentJobId}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
         console.log('Job deleted successfully');
         location.href = '/jobs';
-    } else {
+      } else {
         console.log('Failed to delete job');
-    }
-});
+      }
+    });
+  });
+  
