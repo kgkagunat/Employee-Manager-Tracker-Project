767419674e3c16661715a5401d1c0b727568cb20
Valencia01 <kvalencia.99440@gmail.com>
2023-07-10 03:01:50 -0700

    commented out employee title

--- public/css/create-employee.js
+++ public/css/create-employee.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const newEmployee = {
                 first_name: document.getElementById('new-employee-first-name').value.trim(),
                 last_name: document.getElementById('new-employee-last-name').value.trim(),
-                employee_title: document.getElementById('new-employee-title').value.trim(),
+                // employee_title: document.getElementById('new-employee-title').value.trim(),
                 employee_manager: document.getElementById('new-employee-manager').value.trim(),
                 job_id: document.getElementById('new-employee-job').value.trim(),
                 department_id: document.getElementById('new-employee-department').value.trim(),
--- seeds/employeeSeed.js
+++ seeds/employeeSeed.js
@@ -4,7 +4,7 @@ const employeeData = [
     {
         first_name: 'John',
         last_name: 'Doe',
-        employee_title: 'Lead Engineer',
+        // employee_title: 'Lead Engineer',
         employee_manager: 'Is Manager',
         job_id: 2,
         department_id: 2
@@ -12,7 +12,7 @@ const employeeData = [
     {
         first_name: 'Tom',
         last_name: 'Cruise',
-        employee_title: 'Mechanical Engineer',
+        // employee_title: 'Mechanical Engineer',
         employee_manager: 'John Doe',
         job_id: 4,
         department_id: 2
@@ -20,7 +20,7 @@ const employeeData = [
     {
         first_name: 'Luke',
         last_name: 'Skywalker',
-        employee_title: 'Legal Team Lead',
+        // employee_title: 'Legal Team Lead',
         employee_manager: 'Is Manager',
         job_id: 6,
         department_id: 3
@@ -28,7 +28,7 @@ const employeeData = [
     {
         first_name: 'Darth',
         last_name: 'Vader',
-        employee_title: 'Lawyer',
+        // employee_title: 'Lawyer',
         employee_manager: 'Luke Skywalker',
         job_id: 8,
         department_id: 3
