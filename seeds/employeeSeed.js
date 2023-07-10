a968dd40f84ece7e2da0b675c0cbe7497ab7d7a4
kgkagunat <kgkagunat@gmail.com>
2023-07-09 19:43:52 -1000

    7/9/23 7:41pm - create-employee and employee handlebar created

--- seeds/employeeSeed.js
+++ seeds/employeeSeed.js
@@ -10,13 +10,29 @@ const employeeData = [
         department_id: 2
     },
     {
+        first_name: 'Tom',
+        last_name: 'Cruise',
+        employee_title: 'Mechanical Engineer',
+        employee_manager: 'John Doe',
+        job_id: 4,
+        department_id: 2
+    },
+    {
         first_name: 'Luke',
         last_name: 'Skywalker',
-        employee_title: 'Lawyer',
+        employee_title: 'Legal Team Lead',
         employee_manager: 'Is Manager',
-        job_id: 3,
+        job_id: 6,
         department_id: 3
-    }
+    },
+    {
+        first_name: 'Darth',
+        last_name: 'Vader',
+        employee_title: 'Lawyer',
+        employee_manager: 'Luke Skywalker',
+        job_id: 8,
+        department_id: 3
+    },
 ]
 
 const seedEmployee = () => Employee.bulkCreate(employeeData);
